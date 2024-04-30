550ec3ac5295e351157c3083fb134a2e5824ad71
Fabio Schick <58027418+mrschick@users.noreply.github.com>
2024-04-30 21:29:30 +0200

    Move CfgVehicles to own file

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -24,23 +24,3 @@ class CfgFactionClasses {
     class NO_CATEGORY;
     class jib_marker: NO_CATEGORY { displayName = "Restrict Markers"; };
 };
-
-class CfgVehicles
-{
-    class Module_F;
-    class jib_marker_module: Module_F {
-        isGlobal=1;
-        curatorCanAttach=1;
-        category = "jib_marker";
-    };
-    class jib_marker_moduleDisable: jib_marker_module {
-        scopeCurator=2;
-        displayName = "Disable Restrict Markers";
-        function = "jib_marker_moduleDisable";
-    };
-    class jib_marker_moduleEnable: jib_marker_module {
-        scopeCurator=2;
-        displayName = "Enable Restrict Markers";
-        function = "jib_marker_moduleEnable";
-    };
-};
